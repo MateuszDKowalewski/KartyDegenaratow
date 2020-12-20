d5f2aeaaf724e1680cd76536e13bb0ef72bc311b
aspoka <76015429+aspoka@users.noreply.github.com>
2020-12-20 14:15:41 +0100

    Test changes (#15)
    * Test changes
    
    * Poprawki

--- Assets/Scripts/Cards/CardFactory.cs
+++ Assets/Scripts/Cards/CardFactory.cs
@@ -2,14 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CardFactory : MonoBehaviour
-{
+public class CardFactory : MonoBehaviour{
     
     void Start() {
         // Test comment
     }
     
     void Update() {
-        
+        for (int i = 0; i < 10; i++){
+            Debug.Log(i);
+        }
     }
 }
