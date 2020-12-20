7c003f74dadac120d7105bd057269e50dd27fdb4
aspoka <76015429+aspoka@users.noreply.github.com>
2020-12-20 22:59:13 +0100

    Main reset
    * Test changes (#15)
    
    * Test changes
    
    * Poprawki
    
    * Test-5-1
    
    * Test 5 2
    
    * Test-6-1
    
    * Test 6-2
    
    * Make base version
    
    Co-authored-by: aspoka <76015429+aspoka@users.noreply.github.com>
    
    Co-authored-by: Mateusz Kowalewski <30475686+MateuszDKowalewski@users.noreply.github.com>

--- Assets/Scripts/Cards/Card.cs
+++ Assets/Scripts/Cards/Card.cs
@@ -1,15 +1,13 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Card : MonoBehaviour
 {
 
-    private int y;
-
     void Start()
     {
-        y = 2;
+        
     }
 
     void Update()
