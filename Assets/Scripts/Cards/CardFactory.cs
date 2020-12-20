e86d94a02877ad59b5875b710747e8187f3483cb
aspoka <76015429+aspoka@users.noreply.github.com>
2020-12-20 15:53:38 +0100

    Make base version

--- Assets/Scripts/Cards/CardFactory.cs
+++ Assets/Scripts/Cards/CardFactory.cs
@@ -2,21 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CardFactory : MonoBehaviour{
-    
-    private int x; 
-    private int y;
+public class CardFactory : MonoBehaviour {
 
     void Start() {
-	x = 1;
-        y = 2;
+        
     }
     
     void Update() {
-        for (int i = 0; i < 10; i++){
-            Debug.Log(i);
-        }
-        Debug.Log(x);
-        Debug.Log(y);
+        
     }
 }
