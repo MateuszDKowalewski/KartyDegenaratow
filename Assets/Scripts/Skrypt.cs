c6ffe09bfe90c0f126547658f09ae2e77a492b6d
aspoka <76015429+aspoka@users.noreply.github.com>
2020-12-20 18:54:02 +0100

    Kd 0
    * Dodany skrypt
    
    * test

---
+++ Assets/Scripts/Skrypt.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skrypt : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private int x;
+    void Start()
+    {
+        x = 10;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(x);
+        // test
+    }
+}
