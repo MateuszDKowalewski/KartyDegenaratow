fb2699e433b90d0777a22383ca1e2629bec68b45
MateuszDKowalewski <aspoka1@gmail.com>
2021-01-09 17:25:19 +0100

    Edit checker prefab

--- Assets/Scripts/Arena/CheckerScript.cs
+++ Assets/Scripts/Arena/CheckerScript.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CheckerDestroyer : MonoBehaviour
+public class CheckerScript : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
