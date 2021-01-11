d7b09308c407b891215862c3c01f5faea3c2c94f
MateuszDKowalewski <aspoka1@gmail.com>
2021-01-12 00:17:12 +0100

    Draw line

--- Assets/Scripts/Arena/MouseController.cs
+++ Assets/Scripts/Arena/MouseController.cs
@@ -11,6 +11,9 @@ public class MouseController : MonoBehaviour
 
     public List<Transform> tiles;
 
+    public LineDrawer lineDrawer;
+    private GameObject selectedChackers = null;
+
     void Start()
     {
         tiles = new List<Transform>();
@@ -24,15 +27,19 @@ public class MouseController : MonoBehaviour
     
     void Update()
     {
-        if(Input.GetMouseButtonUp(0))
+        this.tryPlaceChecker();
+        this.tryMoveChecker();
+    }
+
+    private void tryPlaceChecker()
+    {
+        if(Input.GetMouseButtonUp(0) && this.selectedChackers == null)
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit2D hit = Physics2D.Raycast (ray.origin, ray.direction, Mathf.Infinity);
-            Debug.Log(hit.collider.gameObject);
-            Debug.Log(hit.collider.gameObject.GetComponent<TileScript>().canPlayerPut());
-            Debug.Log(!hit.collider.gameObject.GetComponent<TileScript>().isTaken());
             if (hit &&
                 hit.collider.gameObject.transform.parent == map.transform &&
+                hit.collider.gameObject.GetComponent<TileScript>() != null &&
                 hit.collider.gameObject.GetComponent<TileScript>().canPlayerPut() &&
                 !hit.collider.gameObject.GetComponent<TileScript>().isTaken())
             {
@@ -43,4 +50,26 @@ public class MouseController : MonoBehaviour
             }
         }
     }
+
+    private void tryMoveChecker()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast (ray.origin, ray.direction, Mathf.Infinity);
+            if (hit && hit.collider.gameObject.GetComponent<CheckerScript>() != null)
+            {
+                this.selectedChackers = hit.collider.gameObject;
+                lineDrawer.setStart(hit.collider.gameObject.transform.position);
+                lineDrawer.startDraw();
+            }
+        }
+
+        if(Input.GetMouseButtonUp(0))
+        {
+            lineDrawer.reset();
+            this.selectedChackers = null;
+        }
+    }
+
 }
