5c34574e887e0c14f5b3c6540cf8eb84fde150e5
MateuszDKowalewski <aspoka1@gmail.com>
2021-01-13 21:23:04 +0100

    Done sipmle factory

---
+++ Assets/Scripts/Cards/CardEnum.cs
@@ -0,0 +1,10 @@
+﻿public enum CardEnum
+{
+    KOLEGA_ASEKSUALNY,
+    KOLEGA_BISEKSUALNY,
+    KOLEGA_FEMBOY,
+    KOLEGA_FURRY,
+    KOLEGA_GEJ,
+    KOLEGA_NIEBINARNY,
+    
+}
--- Assets/Scripts/Cards/CardFactory.cs
+++ Assets/Scripts/Cards/CardFactory.cs
@@ -2,13 +2,57 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CardFactory : MonoBehaviour {
+public class CardFactory : MonoBehaviour
+{
 
-    void Start() {
-        
+    public GameObject cardPrefab;
+
+    public Canvas canvas;
+
+    public CardPrefabrd kolegaAseksualny;
+    public CardPrefabrd kolegaBiseksualny;
+    public CardPrefabrd kolegaFemboy;
+    public CardPrefabrd kolegaFurru;
+    public CardPrefabrd kolegaGej;
+    public CardPrefabrd kolegaNiebinarny;
+
+    public void generateCard(CardEnum type)
+    {
+        switch(type)
+        {
+            case CardEnum.KOLEGA_ASEKSUALNY:
+            generateCard(kolegaAseksualny);
+                break;
+                
+            case CardEnum.KOLEGA_BISEKSUALNY:
+                generateCard(kolegaBiseksualny);
+                break;
+                
+            case CardEnum.KOLEGA_FEMBOY:
+                generateCard(kolegaFemboy);
+                break;
+                
+            case CardEnum.KOLEGA_FURRY:
+                generateCard(kolegaFurru);
+                break;
+                
+            case CardEnum.KOLEGA_GEJ:
+                generateCard(kolegaGej);
+                break;
+                
+            case CardEnum.KOLEGA_NIEBINARNY:
+                generateCard(kolegaNiebinarny);
+                break;
+                
+        }
     }
-    
-    void Update() {
-        
+
+    private void generateCard(CardPrefabrd prefab) {
+        GameObject card = Instantiate(cardPrefab);
+        if(card.GetComponent<CardDisplay>())
+        {
+            card.GetComponent<CardDisplay>().cardPrefabrd = prefab;
+        }
+        card.transform.parent = canvas.transform;
     }
 }
