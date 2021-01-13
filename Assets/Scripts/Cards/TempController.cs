16bd0d297d5a79764c945f5f125868d1de787024
MateuszDKowalewski <aspoka1@gmail.com>
2021-01-13 21:23:04 +0100

    Done sipmle factory

---
+++ Assets/Scripts/Cards/TempController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempController : MonoBehaviour
+{
+
+    public CardFactory cardFactory;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+            cardFactory.generateCard(CardEnum.KOLEGA_ASEKSUALNY);
+        }
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            cardFactory.generateCard(CardEnum.KOLEGA_BISEKSUALNY);
+        }
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            cardFactory.generateCard(CardEnum.KOLEGA_FEMBOY);
+        }
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            cardFactory.generateCard(CardEnum.KOLEGA_FURRY);
+        }
+        if(Input.GetKeyDown(KeyCode.T))
+        {
+            cardFactory.generateCard(CardEnum.KOLEGA_GEJ);
+        }
+        if(Input.GetKeyDown(KeyCode.Y))
+        {
+            cardFactory.generateCard(CardEnum.KOLEGA_NIEBINARNY);
+        }
+    }
+
+}
