37290c1ff54c91e0c47319106da7a94bedf37f94
Bodziix <76045904+Bodziix@users.noreply.github.com>
2021-01-23 19:58:42 +0100

    new sccene, map, travel between scene, new camera follow, transition between levels (not complete)

---
+++ Assets/Scripts/Map/levelloader.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class levelloader : MonoBehaviour
+{
+    public int iLevelToLoad;
+    public string sLevelToLoad;
+    public bool useIntigerToLoadLevel = false;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        GameObject collisionGameObject = collision.gameObject;
+        if (collisionGameObject.name == "Player")
+        {
+            LoadScene();
+        }
+    }
+
+    void LoadScene()
+    {
+        if (useIntigerToLoadLevel)
+        {
+            SceneManager.LoadScene(iLevelToLoad);
+        }
+        else
+        {
+            SceneManager.LoadScene(sLevelToLoad);
+        }
+    }
+}
+
+
+/*
+                                      |
+                                     /|
+                                    / |
+                                   /  |
+                                  / | |
+                                 / /| |
+                                / / | |                                    
+       _,.-""--._              / /  | |                                      
+     ,'          `.           | '   ' '                                           
+   ,'              `.         ||   / ,|                        .___..--,                  
+  /                  \        ' `.'`.-.,-".  .       _..---""'' __, ,.'                
+ /                    \        \` ."`      `"'\   ,'"_..--''"""'.'.'                     
+/                      \      .'-'             \,' ,'         ,','                               
+|                      |      ,`               ' .`         .' /                 
+|                      |     |           ."".    `-. _____./  /         
+\                      |..---| .".      | | |    .-'""  ,____/     
+ \                   ,'      | |||      | | |    |""`'"'                   
+  `-._   `._.._____  |       | | |      '._.'    |
+      `.   .       `".     ,'|  "    ''          ,+.
+        \  '         |    '  |    .  ..  .        .' `.
+         .'          '    |  ".    ''  ''     ..,'   /
+                   ,'     |    `..        _,''     /
+                  .       \,      `'----''         |
+                  |      ,"|  /                   | '
+                  `     |  | .                 | `,'
+                   .    |  `.|                 |/
+                    `-..'   ,'                .'
+                            | \             ,''
+                            |  `,'.      _,' /
+                            |    | ^.  .'   /
+                             `-'.' |` V    /
+                                   |      /
+                                   |     /
+                                   |   ,'
+                                    `""
+Bodziix was here
+*/
