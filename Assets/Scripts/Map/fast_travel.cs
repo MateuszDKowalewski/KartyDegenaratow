0001d99d9087ff7c7d2eceb34e96e5b4eb5a0327
Bodziix <76045904+Bodziix@users.noreply.github.com>
2021-02-27 18:40:05 +0100

    the fast travel 2 types

---
+++ Assets/Scripts/Map/fast_travel.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fast_travel : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnMouseDown()
+    {
+        if (gameObject.name == "GoToMap")
+        {
+            Application.LoadLevel("Map");
+        }
+        if (gameObject.name == "GoTomap2")
+        {
+            Application.LoadLevel("map2");
+        }
+    }
+}
+/*
+  ____  ____  ____  ____
+ /\   \/\   \/\   \/\   \
+/  \___\ \___\ \___\ \___\
+\  / __/_/   / /   / /   /
+ \/_/\   \__/\/___/\/___/
+   /  \___\    /  \___\
+   \  / __/_  _\  /   /
+    \/_/\   \/\ \/___/
+      /  \__/  \___\
+      \  / _\  /   /
+       \/_/\ \/___/
+         /  \___\
+         \  /   /
+          \/___/
+*/
