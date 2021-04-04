64a9e0a7f1446ebabd364db1cd173b33a1881402
MateuszDKowalewski <aspoka1@gmail.com>
2021-04-05 01:13:59 +0200

    Adding movement

--- Assets/Scripts/Arena/BoardController.cs
+++ Assets/Scripts/Arena/BoardController.cs
@@ -7,19 +7,70 @@ using UnityEngine;
 public class BoardController : MonoBehaviour
 {
     public GameObject checkerPrefab;
+
+    public LineDrawer arrowDrawer;
+    private Vector2Int _movingFrom;
+    private bool moving = false;
+    
     public Row[] rows;
     private GameObject[,] _chackers = new GameObject[4, 4];
 
     private void Update()
     {
-        if (Input.GetKeyUp(KeyCode.Mouse0))
+        putChecker();
+        moveChecker();
+        removeChecker();
+    }
+
+    private void putChecker()
+    {
+        if (Input.GetKeyUp(KeyCode.Q))
         {
             Vector2Int? pos = getMouseToBoardPos();
             if (_chackers != null && pos != null && _chackers[pos.Value.x, pos.Value.y] == null)
             {
-                _chackers[pos.Value.x, pos.Value.y] = Instantiate(checkerPrefab, getField(pos.Value.x, pos.Value.y).transform.position, Quaternion.identity);
                 float scale = getField(pos.Value.x, pos.Value.y).script.scale;
+                _chackers[pos.Value.x, pos.Value.y] = Instantiate(checkerPrefab, getField(pos.Value.x, pos.Value.y).transform.position, Quaternion.identity);
                 _chackers[pos.Value.x, pos.Value.y].transform.localScale = new Vector3(scale, scale, 1);
+            }
+        }
+    }
+
+    private void moveChecker()
+    {
+        Vector2Int? mousePos = getMouseToBoardPos();
+        if (Input.GetKeyDown(KeyCode.Mouse0) && mousePos != null && _chackers[mousePos.Value.x, mousePos.Value.y] != null)
+        {
+            _movingFrom = mousePos.Value;
+            arrowDrawer.startDraw(_chackers[mousePos.Value.x, mousePos.Value.y].transform.position);
+            moving = true;
+        }
+        
+        if (Input.GetKeyUp(KeyCode.Mouse0) && moving)
+        {
+            arrowDrawer.stopDraw();
+            if (mousePos != null && _chackers[mousePos.Value.x, mousePos.Value.y] == null)
+            {
+                GameObject current = _chackers[_movingFrom.x, _movingFrom.y];
+                _chackers[_movingFrom.x, _movingFrom.y] = null;
+                _chackers[mousePos.Value.x, mousePos.Value.y] = current;
+                float scale = getField(mousePos.Value.x, mousePos.Value.y).script.scale;
+                current.transform.position = getField(mousePos.Value.x, mousePos.Value.y).transform.position;
+                current.transform.localScale = new Vector3(scale, scale, 1);
+                moving = false;
+            }
+        }
+    }
+
+    private void removeChecker()
+    {
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            Vector2Int? pos = getMouseToBoardPos();
+            if (_chackers != null && pos != null && _chackers[pos.Value.x, pos.Value.y] != null)
+            {
+                Destroy(_chackers[pos.Value.x, pos.Value.y]);
+                _chackers[pos.Value.x, pos.Value.y] = null;
             }
         }
     }
