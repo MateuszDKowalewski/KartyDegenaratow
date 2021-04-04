7f8b856b6af4dd80b23e0845d8428716e6ab5ede
MateuszDKowalewski <aspoka1@gmail.com>
2021-04-04 22:57:25 +0200

    Transform mouse to board field

--- Assets/Scripts/Arena/CheckerScript.cs
+++ Assets/Scripts/Arena/CheckerScript.cs
@@ -2,16 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CheckerScript : Component
+public class CheckerScript : MonoBehaviour
 {
-    private FieldScript _fieldUnder = null;
 
-    public FieldScript getTileUnder() {
-        return _fieldUnder;
-    }
-
-    public void setTileUnder(FieldScript fieldUnder) {
-        _fieldUnder = fieldUnder;
-        // tileUnder.setTaken(true);
-    }
 }
