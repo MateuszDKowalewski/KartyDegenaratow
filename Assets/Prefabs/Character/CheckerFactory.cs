b9a5b04a799bee2c842ed58b008a9940940eb5e3
MateuszDKowalewski <aspoka1@gmail.com>
2021-04-05 20:39:43 +0200

    Add simple checker factory

---
+++ Assets/Prefabs/Character/CheckerFactory.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckerFactory : MonoBehaviour
+{
+    public GameObject CheckerPrefab;
+    
+    private static Dictionary<string, CharacterPrefab> _allCharacters;
+    private static bool _isInitialized = _allCharacters != null;
+    
+    public void Start()
+    {
+        if (_isInitialized)
+        {
+            return;
+        }
+
+        var prefabs = Resources.LoadAll<CharacterPrefab>("/Assets/Prefabs/Character/Ready");
+        _allCharacters = new Dictionary<string, CharacterPrefab>();
+        foreach (var prefab in prefabs)
+        {
+            _allCharacters.Add(prefab.code, prefab);
+        }
+    }
+
+    public void instantiateChecker(string name, Vector3 position)
+    {
+        if (_allCharacters.ContainsKey(name))
+        {
+            GameObject checker = Instantiate(CheckerPrefab, position, Quaternion.identity);
+            CheckerScript script = checker.GetComponent(typeof(CheckerScript)) as CheckerScript;
+            script.initailize(_allCharacters[name]);
+        }
+    }
+
+    public List<string> getCheckerNames()
+    {
+        List<string> result = new List<string>();
+        foreach (var checker in _allCharacters)
+        {
+            result.Add(checker.Key);
+        }
+        return result;
+    } 
+    
+    
+}
--- Assets/Scripts/Arena/CheckerScript.cs
+++ Assets/Scripts/Arena/CheckerScript.cs
@@ -1,8 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Debug = System.Diagnostics.Debug;
 
 public class CheckerScript : MonoBehaviour
 {
-
+    private Vector2Int[] _possibleMoves;
+    
+    public void initailize(CharacterPrefab prefab)
+    {
+        _possibleMoves = prefab.possibleMoves;
+        SpriteRenderer spriteRenderer = GetComponent(typeof(SpriteRenderer)) as SpriteRenderer;
+        Debug.Assert(spriteRenderer != null, nameof(spriteRenderer) + " != null");
+        spriteRenderer.sprite = prefab.checkerArtwork;
+    }
 }
