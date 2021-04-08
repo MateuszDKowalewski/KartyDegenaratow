28e2ede963647679325774e5954f08ff58440335
Lara9738 <lara.okolska@gmail.com>
2021-04-08 18:06:40 +0200

    jakakolwiek wiadomość

---
+++ Assets/Scripts/Quests/QuestWindow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+
+public class QuestWindow : MonoBehaviour
+{
+    public GameObject questwindow;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        questwindow = GameObject.Find("questwindow");
+        if (questwindow != null)
+        {
+            Debug.Log("dzialam");
+            questwindow.SetActive(false);
+            Debug.Log("dzialam");
+        } 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Quests/barkaQuest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class barkaQuest : Quest
+{
+    public Quest Quest;
+
+    public void papajQuestAktywacja(Collision2D col)
+    {
+        if (Quest.isActive == true)
+        {
+            Debug.Log("OOOOOOOOOO PAAAANIEEEE, TO TY NA MNIE SPOOOOOOJRZAAAŁEEEŚ");
+            Debug.Log("Brawo, zaśpiewałeś barkę z papieżem");
+            Quest.isActive = false;
+        }
+    }
+}
