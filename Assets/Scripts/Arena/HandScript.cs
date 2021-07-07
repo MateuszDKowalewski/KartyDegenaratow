6b0424de4292ba52c94e964b10c85b80032269be
mkowalewski <aspoka1@gmail.com>
2021-07-07 20:06:50 +0200

    Set position of card in hand BUG

--- Assets/Scripts/Arena/HandScript.cs
+++ Assets/Scripts/Arena/HandScript.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class HandScript : MonoBehaviour
 {
 
+    List<GameObject> cards = new List<GameObject>();
     public CardFactory cardFactory;
     
     void Start()
@@ -17,7 +18,19 @@ public class HandScript : MonoBehaviour
         if (Input.GetKeyUp(KeyCode.Space))
         {
             string randomCard = cardFactory.getCardsNames()[0];
-            cardFactory.instantiateCard(randomCard, transform, transform.position);
+            cards.Add(cardFactory.instantiateCard(randomCard, transform, transform.position));
+        }
+
+        updateHorizontalPos();
+    }
+
+    private void updateHorizontalPos()
+    {
+        int n = 0;
+        foreach(GameObject card in cards)
+        {
+            Vector2 temp = new Vector2(-200f + 50 * n++ ,card.transform.position.y);
+            card.transform.position = temp;
         }
     }
 }
