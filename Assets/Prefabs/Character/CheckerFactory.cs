7cea59e5c7e97c7cb92ce6351a7ce7a74ccd36d1
mkowalewski <aspoka1@gmail.com>
2021-07-16 23:25:33 +0200

    Card focus detection ,broken

--- Assets/Prefabs/Character/CheckerFactory.cs
+++ Assets/Prefabs/Character/CheckerFactory.cs
@@ -20,7 +20,6 @@ public class CheckerFactory : MonoBehaviour
         _allCharacters = new Dictionary<string, CharacterPrefab>();
         foreach (var prefab in prefabs)
         {
-            Debug.Log(prefab.code);
             _allCharacters.Add(prefab.code, prefab);
         }
     }
--- Assets/Scripts/Arena/HandScript.cs
+++ Assets/Scripts/Arena/HandScript.cs
@@ -15,17 +15,37 @@ public class HandScript : MonoBehaviour
 
     void Update()
     {
+        drawCard();
+        focusCard();
+        updateHorizontalPos();
+    }
+
+    private void drawCard()
+    {
         if (Input.GetKeyDown(KeyCode.Space))
         {
             string randomCard = deck.getCard();
-            Debug.Log(randomCard);
             GameObject instantiateCard = cardFactory.instantiateCard(randomCard, transform, transform.position);
-            Debug.Log("Instanted:");
-            Debug.Log(instantiateCard);
             cards.Add(instantiateCard);
         }
+    }
 
-        updateHorizontalPos();
+    private void focusCard()
+    {
+        foreach (var card in cards)
+        {
+            BoxCollider2D cardCollider = card.GetComponent(typeof(BoxCollider2D)) as BoxCollider2D;
+            RaycastHit hit = new RaycastHit();
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray.origin,ray.direction, out hit))
+            {
+                BoxCollider2D hitObjest = hit.transform.GetComponent(typeof(BoxCollider2D)) as BoxCollider2D;
+                if (cardCollider.Equals(hitObjest))
+                {
+                    Debug.Log(card.name);
+                }
+            }
+        }
     }
 
     private void updateHorizontalPos()
